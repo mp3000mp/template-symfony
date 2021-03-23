e3a353d540878d0146d6990196da25ecadb6a3e6
Matthias Perret <moussadedijon@gmail.com>
2021-03-23 02:12:33 +0100

    e2e coverage

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    ['istanbul', {
+      extension: ['.js', '.ts', '.vue']
+      // include: ['src/**/*.{js,ts,vue}']
+    }]
   ]
 }
